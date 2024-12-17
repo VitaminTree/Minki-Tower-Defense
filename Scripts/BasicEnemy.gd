7b7ad72980fc327d2bfbd930f23a600f6c0eb7b2
VitaminTree <jbgonzales510@gmail.com>
2024-12-16 22:47:01 -0800

    Creation of Enemy class
    Learned about how classes work in gdscript, so I implemented it with the two enemy types in the game, Wisps and Dragoons. They're both identical save for a few variables, so a basic enemy type covers both of them.
    
    Additionally, enemy health bar functionality was moved into the health setter, so the health bar no longer checks for updates every physics cycle.

--- Scripts/BasicEnemy.gd
+++ Scripts/BasicEnemy.gd
@@ -1,17 +1,42 @@
-extends CharacterBody2D
+class_name enemy extends CharacterBody2D
 
+@onready var progress_bar: TextureProgressBar = $ProgressBar
+
+# I want the health of an enemy type to be editable via the Inspector, so an @export is needed
+# I also want the health bar to be modified via the setter, so that it doesnt check for an update every cycle
+# However, if the health is modified via the Inspector, then the setter is called while being instantiated
+# 	before the health bar exists, resulting in an error.
+# SOLUTION: Another variable "healthpoints" is created as a variable to be exported.
+# 	The enemy has no health when being created, but is assigned the value of healthpoints when entering the scene
+#	via the _ready() function
+var health: int:
+	get:
+		return health
+	set(value): 
+		health = value
+		progress_bar.value = value
+		if health <= 0:
+			SignalMessenger.MONEY_PAYMENT.emit(payment)
+			get_parent().queue_free()
+			return
+		if health < progress_bar.max_value:
+			progress_bar.visible = true
+
+@export var healthpoints: int = 1
 @export var speed = 200
-@export var health = 2
-@export var damage = 5
-@export var payment = 25
+@export var damage = 1
+@export var payment = 5
+
+
+
+func _ready() -> void:
+	health = healthpoints
+	progress_bar.max_value = health
+	progress_bar.visible = false
 
 func _process(delta: float) -> void:
 	var path = get_parent()
 	path.set_progress(path.get_progress()+speed*delta)
 	if path.get_progress_ratio() == 1:
 		SignalMessenger.HEALTH_UPDATE.emit(-1*damage)
-		get_parent().queue_free()
-	
-	if health <= 0:
-		SignalMessenger.MONEY_PAYMENT.emit(payment)
 		get_parent().queue_free()
