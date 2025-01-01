a66285e1f5bfb564a499ce2fa3d90b62d3fda99e
VitaminTree <jbgonzales510@gmail.com>
2024-12-31 21:06:14 -0800

    Renaming and relocation of certain scripts
    LevelOne renamed to LevelOneTerrain, as it emits signals if mouse is over certain level terrain
    BespokePause renamed to PauseButton, pause buttons are generally needed and are not temporary features
    
    SignalMessenger moved to a folder that will hold autoloaded scripts
    Created a new autoloaded script, GameData, but it doesn't do anything yet

---
+++ Scripts/Autoloads/GameData.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# At some point, every relevant resource that's tied to the "player" should probably be in here.
+# This includes but is not limited to:
+#  - Inventory
+#  - Money
+#  - Wave progress
+#  - Health
+#  - Experience? Idk
+#
+# For now, this script will remain empty until the next realease is shipped, then I may consider refactoring
